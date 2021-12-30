3170e6fb02280e55a03d0e95edf8ff28f5a2cf17
Anmol Sahoo <anmol.sahoo25@gmail.com>
2021-12-30 12:54:39 -0500

    added instrumentation for reads/writes

--- asmcomp/thread_sanitizer.ml
+++ asmcomp/thread_sanitizer.ml
@@ -1,7 +1,34 @@
 open Cmm
 
+let select_function read_or_write size =
+  let acc_string = if read_or_write then "read" else "write" in
+  match size with
+  | Byte_unsigned
+  | Byte_signed -> Printf.sprintf "__tsan_%s1" acc_string
+  | Sixteen_unsigned
+  | Sixteen_signed -> Printf.sprintf "__tsan_%s4" acc_string
+  | Thirtytwo_unsigned
+  | Thirtytwo_signed -> Printf.sprintf "__tsan_%s8" acc_string
+  | Word_int
+  | Word_val -> Printf.sprintf "__tsan_%s16" acc_string
+  | Single -> Printf.sprintf "__tsan_%s8" acc_string
+  | Double -> Printf.sprintf "__tsan_%s16" acc_string
+
 let instrument label body =
   let dbg = Debuginfo.none in
+  let rec aux = function
+    | Cop(Cload {memory_chunk; }, args, _) as c ->
+        Csequence(Cop(Cextcall(select_function true memory_chunk, typ_void, [], false),
+        [List.hd args], dbg), c)
+    | Cop(Cstore(memory_chunk, init_or_assn), args, _) as c ->
+        begin match init_or_assn with
+        | Assignment -> 
+            Csequence(Cop(Cextcall(select_function false memory_chunk, typ_void, [], false),
+            [List.hd args], dbg), c)
+        | _ -> c
+        end
+    | Csequence(c1,c2) -> Csequence(aux c1, aux c2)
+    | _ as c -> c in
   let entry_instr = Cop(
     Cextcall("__tsan_func_entry", typ_void, [], false),
     [Cconst_symbol(label, dbg)],
@@ -12,4 +39,4 @@ let instrument label body =
     dbg) in
   Csequence(
     entry_instr,
-    Csequence(body, exit_instr))
+    Csequence(aux body, exit_instr))
