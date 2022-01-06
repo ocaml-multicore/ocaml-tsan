740446140de826aa1f3aae9e7a77f20145f8fd5a
Anmol Sahoo <anmol.sahoo25@gmail.com>
2022-01-05 23:25:03 -0500

    added extra cases and asmlink

--- asmcomp/thread_sanitizer.ml
+++ asmcomp/thread_sanitizer.ml
@@ -17,18 +17,37 @@ let select_function read_or_write size =
 let instrument label body =
   let dbg = Debuginfo.none in
   let rec aux = function
-    | Cop(Cload {memory_chunk; }, args, _) as c ->
+    | Cop (Cload {memory_chunk; }, args, _) as c ->
         Csequence(Cop(Cextcall(select_function true memory_chunk, typ_void, [], false),
         [List.hd args], dbg), c)
-    | Cop(Cstore(memory_chunk, init_or_assn), args, _) as c ->
+    | Cop (Cstore(memory_chunk, init_or_assn), args, _) as c ->
         begin match init_or_assn with
         | Assignment -> 
             Csequence(Cop(Cextcall(select_function false memory_chunk, typ_void, [], false),
             [List.hd args], dbg), c)
         | _ -> c
         end
+    | Cop (op, es, dbg) -> Cop (op, List.map aux es, dbg)
+    | Clet (v, e, body) -> Clet (v, aux e, aux body)
+    | Clet_mut (v, k, e, body) -> Clet_mut (v, k, aux e, aux body)
+    | Cphantom_let (v, e, body) -> Cphantom_let (v, e, aux body)
+    | Cassign (v, e) -> Cassign (v, aux e)
+    | Ctuple es -> Ctuple (List.map aux es)
     | Csequence(c1,c2) -> Csequence(aux c1, aux c2)
-    | _ as c -> c in
+    | Ccatch (isrec, cases, body) ->
+        let cases = List.map (fun (nfail, ids, e, dbg) -> (nfail, ids, aux e, dbg)) cases in
+        Ccatch (isrec, cases, aux body)
+    | Cexit (ex, args) -> Cexit (ex, List.map aux args)
+    | Cifthenelse (cond, t_dbg, t, f_dbg, f, dbg) ->
+        Cifthenelse (aux cond, t_dbg, aux t, f_dbg, aux f, dbg)
+    | Ctrywith (e, ex, handler, dbg) ->
+        Ctrywith (aux e, ex, aux handler, dbg)
+    | Cswitch (e, cases, handlers, dbg) ->
+        let handlers = Array.map (fun (handler, handler_dbg) -> (aux handler, handler_dbg)) handlers in
+        Cswitch(aux e, cases, handlers, dbg)
+    (* no instrumentation *)
+    | Cconst_int _ | Cconst_natint _ | Cconst_float _
+    | Cconst_symbol _ | Cvar _ as c -> c in
   let entry_instr = Cop(
     Cextcall("__tsan_func_entry", typ_void, [], false),
     [Cconst_symbol(label, dbg)],
