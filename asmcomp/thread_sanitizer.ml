3ccb74b8dd6b36335104c9152e9115f858de38fb
Olivier Nicole <olivier@chnik.fr>
2022-05-05 17:41:05 +0200

    Fix possible problems in TSan instrumentation
    This addresses two things that I saw as possibly erroneous in the TSan
    instrumentation:
    - The calls to `__tsan_readN` and `__tsan_writeN` seemed to have the
      wrong N (size of the accessed region in bytes) in some cases.
    - The emitted calls to `__tsan_*` functions could cause the same memory
      location to be accessed twice. I'm not sure whether this is an issue
      or not in this context.
    - As the sparsely available documentation suggests, a call to
      `__tsan_init` is performed at the beginning of every compilation unit
      (this function can be called several times, only the first call has an
      effect).

--- asmcomp/thread_sanitizer.ml
+++ asmcomp/thread_sanitizer.ml
@@ -1,30 +1,55 @@
 open Cmm
+module V = Backend_var
+module VP = Backend_var.With_provenance
+
+type read_or_write = Read | Write
+
+let init_code () =
+  Cop (Cextcall ("__tsan_init", typ_void, [], false), [], Debuginfo.none)
 
 let select_function read_or_write size =
-  let acc_string = if read_or_write then "read" else "write" in
+  let acc_string = match read_or_write with Read -> "read" | Write -> "write" in
   match size with
   | Byte_unsigned
   | Byte_signed -> Printf.sprintf "__tsan_%s1" acc_string
   | Sixteen_unsigned
-  | Sixteen_signed -> Printf.sprintf "__tsan_%s4" acc_string
+  | Sixteen_signed -> Printf.sprintf "__tsan_%s2" acc_string
   | Thirtytwo_unsigned
-  | Thirtytwo_signed -> Printf.sprintf "__tsan_%s8" acc_string
+  | Thirtytwo_signed -> Printf.sprintf "__tsan_%s4" acc_string
   | Word_int
-  | Word_val -> Printf.sprintf "__tsan_%s16" acc_string
-  | Single -> Printf.sprintf "__tsan_%s8" acc_string
-  | Double -> Printf.sprintf "__tsan_%s16" acc_string
+  | Word_val -> Printf.sprintf "__tsan_%s%d" acc_string (Sys.word_size / 8)
+  | Single -> Printf.sprintf "__tsan_%s4" acc_string
+  | Double -> Printf.sprintf "__tsan_%s8" acc_string
 
 let instrument label body =
   let dbg = Debuginfo.none in
   let rec aux = function
-    | Cop (Cload {memory_chunk; }, args, _) as c ->
-        Csequence(Cop(Cextcall(select_function true memory_chunk, typ_void, [], false),
-        [List.hd args], dbg), c)
-    | Cop (Cstore(memory_chunk, init_or_assn), args, _) as c ->
+    | Cop (Cload {memory_chunk; _} as load_op, args, dbginfo) ->
+        let loc = List.hd args in
+        let loc_id = VP.create (V.create_local "loc") in
+        let loc_exp = Cvar (VP.var loc_id) in
+        let args = loc_exp :: List.tl args in
+        Clet (loc_id, loc,
+          Csequence
+            (Cop
+              (Cextcall
+                (select_function Read memory_chunk, typ_void, [], false),
+                [loc_exp], dbg),
+            Cop (load_op, args, dbginfo)))
+    | Cop (Cstore(memory_chunk, init_or_assn), args, dbginfo) as c ->
         begin match init_or_assn with
-        | Assignment -> 
-            Csequence(Cop(Cextcall(select_function false memory_chunk, typ_void, [], false),
-            [List.hd args], dbg), c)
+        | Assignment ->
+            let loc = List.hd args in
+            let loc_id = VP.create (V.create_local "loc") in
+        let loc_exp = Cvar (VP.var loc_id) in
+            let args = loc_exp :: List.tl args in
+            Clet (loc_id, loc,
+              Csequence
+                (Cop (Cextcall
+                       (select_function Write memory_chunk, typ_void, [],
+                         false),
+                       [loc_exp], dbg),
+                Cop (Cstore (memory_chunk, init_or_assn), args, dbginfo)))
         | _ -> c
         end
     | Cop (op, es, dbg) -> Cop (op, List.map aux es, dbg)
@@ -47,7 +72,8 @@ let instrument label body =
         Cswitch(aux e, cases, handlers, dbg)
     (* no instrumentation *)
     | Cconst_int _ | Cconst_natint _ | Cconst_float _
-    | Cconst_symbol _ | Cvar _ as c -> c in
+    | Cconst_symbol _ | Cvar _ as c -> c
+  in
   let entry_instr = Cop(
     Cextcall("__tsan_func_entry", typ_void, [], false),
     [Cconst_symbol(label, dbg)],
