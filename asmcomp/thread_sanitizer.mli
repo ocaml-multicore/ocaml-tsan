c055437706032c412c02762b7f3db8d4f0022793
Olivier Nicole <olivier@chnik.fr>
2022-05-06 17:39:24 +0200

    Add missing copyright headers

--- asmcomp/thread_sanitizer.mli
+++ asmcomp/thread_sanitizer.mli
@@ -1,2 +1,17 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                      Anmol Sahoo, Purdue University                    *)
+(*                                                                        *)
+(*   Copyright 2022 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
 val instrument : string -> Cmm.expression -> Cmm.expression
 val init_code : unit -> Cmm.expression
