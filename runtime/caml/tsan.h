2f3fb55b6471e8900e40d6524a43f1d24baefbbf
fabbing <fabrice@tarides.com>
2022-09-01 18:57:22 +0200

    Call tsan func exit with libunwind from C
    Co-authored-by: Olivier Nicole <olivier@chnik.fr>

---
+++ runtime/caml/tsan.h
@@ -0,0 +1,22 @@
+/**************************************************************************/
+/*                                                                        */
+/*                                 OCaml                                  */
+/*                                                                        */
+/* TODO                                                                   */
+/*                                                                        */
+/*   All rights reserved.  This file is distributed under the terms of    */
+/*   the GNU Lesser General Public License version 2.1, with the          */
+/*   special exception on linking described in the file LICENSE.          */
+/*                                                                        */
+/**************************************************************************/
+
+#ifndef CAML_TSAN_H
+#define CAML_TSAN_H
+
+#ifdef CAML_INTERNALS
+
+void caml_tsan_exn_func_exit_c(char* limit);
+
+#endif /* CAML_INTERNALS */
+
+#endif /* CAML_TSAN_H */
