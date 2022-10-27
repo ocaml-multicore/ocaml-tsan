8e40238c3927a255d84f9668cf99a993f5d9f503
Fabrice Buoro <fabrice@tarides.com>
2022-10-27 15:05:44 +0200

    Update backtrace post-processing in tests
    This make TSan tests more portable by supporting more variations of
    TSan's backtrace report format (i.e. the number of code locations correctly
    symbolized varies from machine to machine).

--- testsuite/tests/tsan/filter-locations.sh
+++ testsuite/tests/tsan/filter-locations.sh
@@ -1,25 +1,33 @@
 #!/bin/sh
-
 set -eu
-
-# https://stackoverflow.com/questions/29613304/is-it-possible-to-escape-regex-metacharacters-reliably-with-sed/29626460#29626460
-progpath_escaped=$(echo ${program} | sed 's/[^^\\]/[&]/g; s/\^/\\^/g; s/\\/\\\\/g')
-progname_escaped=$(echo ${program} | sed 's/^.*\/\([^\/]\+\)/\1/' | sed 's/\\/\\\\/g')
-regex_trim_fun='\(caml[a-zA-Z_0-9]\+\.[a-zA-Z_0-9]\+\)_[[:digit:]]\+'
-regex_mem_location='of size \([0-9]\+\) at 0x[0-9a-f]\+'
-regex_mutex_location='M\([0-9]\+\) (0x[0-9a-f]\+)'
 
 # - Remove mangling of functions (NOTE: functions of the same name, or
 #   anonymous functions, become indistinguishable) and replace it with
 #   '<implemspecific>'
-# - Replace hexadecimal locations with '<implemspecific>'
+# - Replace file+hexadecimal locations with '<implemspecific>'
 # - Replace the complete path of the program by '<systemspecific>/' followed by
 #   the program filename.
-sed -e \
-  "s/+0x[0-9a-f]\+)/+<implemspecific>)/
-   s/pid=[0-9]\+/pid=<implemspecific>/
-   s/tid=[0-9]\+/tid=<implemspecific>/
-   s/${regex_mem_location}/of size \1 at <implemspecific>/
-   s/${regex_trim_fun}/\1_<implemspecific>/
-   s/${regex_mutex_location}/M\1 (<implemspecific>)/
-   s/${progpath_escaped}/<systemspecific>\/${progname_escaped}/"
+script='s/pid=[0-9]\+/pid=<implemspecific>/
+s/tid=[0-9]\+/tid=<implemspecific>/
+
+/\([Rr]ead\|[Ww]rite\) of size/ {
+  s/of size \([0-9]\+\) at 0x[0-9a-f]\+/of size \1 at <implemspecific>/
+}
+
+/Mutex M.* created at:/ {
+  s/M\([0-9]\+\) (0x[0-9a-f]\+)/M\1 (<implemspecific>)/
+}
+
+/#[0-9]\+/ {
+  s/\(#[0-9]\+\) \([^ ]*\) [^ ]* (\([^ ]*\))/\1 \2 <implemspecific> (\3)/
+  s/\(caml[a-zA-Z_0-9]\+\.[a-zA-Z_0-9]\+\)_[[:digit:]]\+/\1_<implemspecific>/
+  s/(\(.\+\)+0x[0-9a-f]\+)/(<implemspecific>)/
+}
+
+/SUMMARY/ {
+  s/data race (.*\/\(.\+\)+0x[0-9a-f]\+) in /data race (<systemspecific>\/\1+<implemspecific>) in /
+  s/data race .*\/\(.\+\):[[:digit:]]\+ in /data race (<systemspecific>\/\1+<implemspecific>) in /
+  s/\(caml[a-zA-Z_0-9]\+\.[a-zA-Z_0-9]\+\)_[[:digit:]]\+/\1_<implemspecific>/
+}'
+
+sed -e "${script}"
