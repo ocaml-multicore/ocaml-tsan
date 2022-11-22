4ba74593c320d862272c6725d5f98bfdaf1ca85b
fabbing <fabrice@tarides.com>
2022-11-22 16:45:17 +0100

    Disable TSan instrumentation in GC tracing
    TSan reports from the runtime clog the user report.
    
    Co-authored-by: Olivier Nicole <olivier@tarides.com>

--- runtime/major_gc.c
+++ runtime/major_gc.c
@@ -658,6 +658,10 @@ void caml_shrink_mark_stack (void)
 
 void caml_darken_cont(value cont);
 
+CAMLno_tsan /* FIXME TSan race reports from this function clog user programs,
+               so we disable instrumentation here. However, Further
+               investigation would be needed about the cause of these race
+               reports. */
 static void mark_slice_darken(struct mark_stack* stk, value v, mlsize_t i,
                               intnat* work)
 {
@@ -701,6 +705,10 @@ static void mark_slice_darken(struct mark_stack* stk, value v, mlsize_t i,
   }
 }
 
+CAMLno_tsan /* FIXME TSan race reports from this function clog user programs,
+               so we disable instrumentation here. However, Further
+               investigation would be needed about the cause of these race
+               reports. */
 static intnat do_some_marking(intnat budget) {
   struct mark_stack* stk = Caml_state->mark_stack;
   while (stk->count > 0) {
