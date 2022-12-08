6c060b0d5d1d0e7e21f5beeeb6fcbafb0cbab81a
Olivier Nicole <olivier@chnik.fr>
2022-12-08 15:27:02 +0100

    Fix build with --enable-tsan on darwin

--- utils/config.mli
+++ utils/config.mli
@@ -245,6 +245,10 @@ val afl_instrument : bool
 val tsan : bool
 (** Whether ThreadSanitizer instrumentation is enabled *)
 
+val tsan_ld_flags : string
+(* Flags to pass to the system linker when build ThreadSanitizer-instrumented
+   programs *)
+
 
 (** Access to configuration values *)
 val print_config : out_channel -> unit
