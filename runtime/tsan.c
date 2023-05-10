1cec41e4499269df4961be87db0641f01fc5e7f5
Olivier Nicole <olivier@chnik.fr>
2023-05-10 14:01:49 +0200

    Treat accesses to volatile values as relaxed atomics
    See https://github.com/ocaml-multicore/ocaml-tsan/issues/25
    
    Co-authored-by: Fabrice Buoro <fabrice@tarides.com>

--- runtime/tsan.c
+++ runtime/tsan.c
@@ -14,6 +14,8 @@
 
 #define CAML_INTERNALS
 
+#ifdef NATIVE_CODE
+
 #ifndef DEBUG
 #undef TSAN_DEBUG
 #endif
@@ -179,3 +181,54 @@ CAMLno_tsan void caml_tsan_func_entry_on_resume(uintnat pc, char* sp,
   caml_tsan_debug_log_pc("forced__tsan_func_entry for", pc);
   __tsan_func_entry((void*)next_pc);
 }
+
+#endif // NATIVE_CODE
+
+#include "caml/mlvalues.h"
+#include <stdbool.h>
+
+Caml_inline bool is_aligned(void *ptr, size_t byte_count)
+{
+  return (uintptr_t)ptr % byte_count == 0;
+}
+
+#include <stdint.h>
+
+extern uint8_t __tsan_atomic8_load(void*, int);
+extern uint16_t __tsan_atomic16_load(void*, int);
+extern uint32_t __tsan_atomic32_load(void*, int);
+extern uint64_t __tsan_atomic64_load(void*, int);
+extern unsigned __int128 __tsan_atomic128_load(void*, int);
+
+#define DEFINE_TSAN_VOLATILE_READ_WRITE(size, bitsize)                                  \
+   \
+extern void __tsan_read##size(void*);       \
+extern void __tsan_write##size(void*);     \
+     \
+  void __tsan_volatile_read##size(void *ptr)                             \
+  {                                                                      \
+    const bool is_atomic = size <= sizeof(long long) &&            \
+               is_aligned(ptr, 8);   \
+    if (is_atomic)                                                 \
+      __tsan_atomic##bitsize##_load(ptr, memory_order_relaxed);                                                \
+    else       \
+      __tsan_read##size(ptr);            \
+  }                                                                      \
+  void __tsan_unaligned_volatile_read##size(void *ptr)                   \
+  {    \
+    __tsan_volatile_read##size(ptr);                           \
+  }     \
+  void __tsan_volatile_write##size(void *ptr)                            \
+  {                                                                      \
+    __tsan_write##size(ptr);     \
+  }                                                                      \
+  void __tsan_unaligned_volatile_write##size(void *ptr)                  \
+  {    \
+    __tsan_volatile_write##size(ptr);                          \
+  }
+
+DEFINE_TSAN_VOLATILE_READ_WRITE(1, 8);
+DEFINE_TSAN_VOLATILE_READ_WRITE(2, 16);
+DEFINE_TSAN_VOLATILE_READ_WRITE(4, 32);
+DEFINE_TSAN_VOLATILE_READ_WRITE(8, 64);
+DEFINE_TSAN_VOLATILE_READ_WRITE(16, 128);
